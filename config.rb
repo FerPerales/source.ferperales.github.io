dba61e0224d67b2d44a63994dc719d97b93d2dc5
Fer Perales <me@ferperales.net>
2016-08-07 12:33:55 -0500

    Downgrade to Middleman 3

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+activate :dotenv
 activate :directory_indexes
 
 configure :development do
@@ -12,4 +13,11 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
+end
+
+activate :cloudfront do |cf|
+  cf.access_key_id = 'I'
+  cf.secret_access_key = 'love'
+  cf.distribution_id = 'cats'
+  # cf.after_build = false  # default is false
 end
