0a2c52cd5aa92b4f05ee418a6a3eb9e508d52538
Fer Perales <me@ferperales.net>
2020-10-08 22:04:02 -0500

    Remove unnecesary config

--- config.rb
+++ config.rb
@@ -20,33 +20,6 @@ configure :build do
   activate :asset_hash
 end
 
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = ENV['AWS_BUCKET']
-  s3_sync.region                     = 'us-west-1'
-  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY']
-  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET']
-  s3_sync.delete                     = false
-  s3_sync.after_build                = false
-  s3_sync.prefer_gzip                = true
-  s3_sync.path_style                 = true
-  s3_sync.reduced_redundancy_storage = false
-  s3_sync.acl                        = 'public-read'
-  s3_sync.encryption                 = false
-  s3_sync.prefix                     = ''
-  s3_sync.version_bucket             = false
-  s3_sync.index_document             = 'index.html'
-end
-caching_policy 'text/html', max_age: 0, must_revalidate: true
-default_caching_policy max_age: (60 * 60 * 24 * 365)
-
-activate :deploy do |deploy|
-  deploy.deploy_method       = :rsync
-  deploy.host                = "#{ENV['SERVER_USER']}@#{ENV['SERVER_HOST']}"
-  deploy.path                = ENV['SERVER_PATH']
-  deploy.user                = ENV['SERVER_USER']
-  deploy.clean               = true
-end
-
 set :markdown_engine, :redcarpet
 set :markdown,
     fenced_code_blocks: true,
